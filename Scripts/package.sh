0121cf554744796f3632d179c411012d7cddf0aa
Randy Holobaugh <rholobaugh@gmail.com>
2026-09-13 14:52:51 -0500

    Add app icon

--- Scripts/package.sh
+++ Scripts/package.sh
@@ -31,6 +31,7 @@ mkdir -p "$MACOS_DIR" "$RESOURCES_DIR"
 
 cp "$BUILD_DIR/$CONFIGURATION/$APP_NAME" "$MACOS_DIR/$APP_NAME"
 cp "$ROOT_DIR/Support/Info.plist" "$CONTENTS_DIR/Info.plist"
+cp "$ROOT_DIR/Support/AppIcon.icns" "$RESOURCES_DIR/AppIcon.icns"
 printf "APPL????" > "$CONTENTS_DIR/PkgInfo"
 chmod +x "$MACOS_DIR/$APP_NAME"
 
